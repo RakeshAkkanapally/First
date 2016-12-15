9f9cb782421c2a0af40359fe30be3ad6fd808485
RakeshAkkanapally <rakeshcharya@gmail.com>
2016-12-15 21:26:01 +0530

    updated login.java

--- src/test/java/com/npi/pageobjects/Login.java
+++ src/test/java/com/npi/pageobjects/Login.java
@@ -24,7 +24,7 @@ public class Login {
 		Selenium.sendKeys(password,string2);
 		Selenium.click(Signin);
 		SoftAssert sa=new SoftAssert();
-		sa.assertEquals("", "");
+		sa.assertEquals("Hi", "Hi");
 		sa.assertAll();
 	}
 	
